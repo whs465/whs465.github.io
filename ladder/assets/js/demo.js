1e8a411213ef57f59a22d60b9c6a111b733a62e8
whs465 <whs465@gmail.com>
2020-11-14 10:10:15 +0300

    ladder

--- ladder/assets/js/demo.js
+++ ladder/assets/js/demo.js
@@ -432,12 +432,13 @@ var chartData = {
     '16',
     '17',
     '18',
+    '19',
   ],
   datasets: [
     {
       label: 'Singles',
       fill: false,
-      data: [0, 0, 1, 1, 1, 2, 2, 3, 3, 6, 7, 7, 7, 7, 7, 7, 7],
+      data: [0, 0, 1, 1, 1, 2, 2, 3, 3, 6, 7, 7, 7, 7, 7, 7, 7, 7],
       backgroundColor: 'transparent',
       borderColor: colors[0],
       borderWidth: 2,
@@ -446,7 +447,7 @@ var chartData = {
     {
       label: 'Doubles',
       fill: false,
-      data: [0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5, 5],
+      data: [0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5, 5, 5],
       backgroundColor: colors[3],
       borderColor: colors[1],
       borderWidth: 2,
